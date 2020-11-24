fd25c0c75849148f0892fa2fa858a957f64393be
Joerg Faschingbauer <jf@faschingbauer.co.at>
2020-11-24 12:12:10 +0100

    uniq reuirements defined

---
+++ livehacking/uniq.py
@@ -0,0 +1,9 @@
+def uniq(sequence):
+    # hier geschieht ein wunder
+    return []
+
+input_sequence = [5, 2, 3, 5, 6, 2, 11, 2, 9, 100, 1, 100]
+
+output_sequence = uniq(input_sequence)
+#output_sequence = [5, 2, 3, 6, 11, 9, 100, 1]
+
