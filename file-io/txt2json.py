bc18b0f009ccb2d23f9d67c36f330dcc9f183c6a
Joerg Faschingbauer <jf@faschingbauer.co.at>
2020-11-25 14:42:40 +0100

    requirements defined :-)

--- file-io/txt2json.py
+++ file-io/txt2json.py
@@ -5,16 +5,33 @@ import sys
 inputfile = sys.argv[1]
 outputfile = sys.argv[2]
 
+# read input file into list of records
+# ------------------------------------
+
+# TODO:
+
+# records = read_records_from_txt_file(inputfile)
+
 records = []
 with open(inputfile) as fin:
     for line in fin:
-        fields = line.split(':')
+        if line.isspace():
+            continue
+
+        fields = line.rstrip('\n').split(':')
         record = {
             'name': fields[0],
             'birthday': fields[1],
             'birthplace': fields[2],
         }
         records.append(record)
+
+# write records into json output file
+# -----------------------------------
+
+# TODO:
+
+# write_records_to_json_file(records, outputfile)
 
 with open(outputfile, 'x') as fout:
     json.dump(records, fout)
